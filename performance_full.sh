d63743aaed8cdf90a00e0734cdabd2ec08060692
NiksaRamljak <niksa.ramljak@fpmoz.sum.ba>
2025-06-29 17:05:04 +0200

    update performance scripts and gitignore

--- performance_full.sh
+++ performance_full.sh
@@ -5,14 +5,28 @@ mkdir -p output
 
 for img in ./test_jpeg/*.{jpg,jpeg}; do
     img_name=$(basename "$img" .${img##*.})
+    log_file="./performance_logs/statsfor${img_name}-one.txt"
+    perf stat -r 5 -o "$log_file" ./build/fltr_sobel_test "$img" "./output/${img_name}-one.jpg" -t 1
+    echo "Processed $img, stats saved to $log_file"
+done
+
+for img in ./test_pgm/*.pgm; do
+    img_name=$(basename "$img" .pgm)
+    log_file="./performance_logs/statsfor${img_name}-one.txt"
+    perf stat -r 5 -o "$log_file" ./build/fltr_sobel_test "$img" "./output/${img_name}-one.pgm" -t 1
+    echo "Processed $img, stats saved to $log_file"
+done
+
+for img in ./test_jpeg/*.{jpg,jpeg}; do
+    img_name=$(basename "$img" .${img##*.})
     log_file="./performance_logs/statsfor${img_name}.txt"
-    perf stat -o "$log_file" ./build/fltr_sobel_test "$img" "./output/${img_name}.jpg"
+    perf stat -r 5 -o "$log_file" ./build/fltr_sobel_test "$img" "./output/${img_name}.jpg"
     echo "Processed $img, stats saved to $log_file"
 done
 
 for img in ./test_pgm/*.pgm; do
     img_name=$(basename "$img" .pgm)
     log_file="./performance_logs/statsfor${img_name}.txt"
-    perf stat -o "$log_file" ./build/fltr_sobel_test "$img" "./output/${img_name}.pgm"
+    perf stat -r 5 -o "$log_file" ./build/fltr_sobel_test "$img" "./output/${img_name}.pgm"
     echo "Processed $img, stats saved to $log_file"
 done
--- performance_noext.sh
+++ performance_noext.sh
@@ -6,6 +6,12 @@ mkdir -p output
 for img in ./test_pgm/*.pgm; do
     img_name=$(basename "$img" .pgm)
     log_file="./performance_logs/statsfor${img_name}-pure.txt"
-    perf stat -o "$log_file" ./build/fltr_sobel "$img" "./output/${img_name}.pgm"
+    perf stat -r 5 -o "$log_file" ./build/fltr_sobel "$img" "./output/${img_name}-pure.pgm"
+    echo "Processed $img, stats saved to $log_file"
+done
+for img in ./test_pgm/*.pgm; do
+    img_name=$(basename "$img" .pgm)
+    log_file="./performance_logs/statsfor${img_name}-pure-one.txt"
+    perf stat -r 5 -o "$log_file" ./build/fltr_sobel "$img" "./output/${img_name}-pure-one.pgm" -t 1
     echo "Processed $img, stats saved to $log_file"
 done
